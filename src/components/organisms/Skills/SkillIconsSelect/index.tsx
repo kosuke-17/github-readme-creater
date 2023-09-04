d8902f6fe71c38583ec38af0309f9709ed41663e
kosuke-17 <tamutamu17@icloud.com>
2023-09-04 09:13:09 +0900

    他の言語も追加できるように修正

--- src/components/organisms/Skills/SkillIconsSelect/index.tsx
+++ src/components/organisms/Skills/SkillIconsSelect/index.tsx
@@ -33,11 +33,77 @@ const tabs = [
 ]
 
 const programmingSkills = [
+  { label: 'Aiscript', value: 'aiscript' },
   { label: 'TypeScript', value: 'ts' },
   { label: 'Javascript', value: 'js' },
   { label: 'Python', value: 'py' },
   { label: 'Java', value: 'java' },
   { label: 'PHP', value: 'php' },
+  { label: 'C', value: 'c' },
+  { label: 'C#', value: 'cs' },
+  { label: 'C++', value: 'cpp' },
+  { label: 'Crystal', value: 'crystal' },
+  { label: 'Clojure', value: 'clojure' },
+  { label: 'Coffeescript', value: 'coffeescript' },
+  { label: 'CSS', value: 'css' },
+  { label: 'Dart', value: 'dart' },
+  { label: '', value: '' },
+]
+
+const frontendSkills = [
+  { label: 'Alpinejs', value: 'alpinejs' },
+  { label: 'Angular', value: 'angular' },
+  { label: 'Astro', value: 'astro' },
+  { label: 'D3', value: 'd3' },
+  { label: '', value: '' },
+]
+const backendSkills = [
+  { label: 'Actix', value: 'actix' }, // rustのフレームワーク
+  { label: 'Ableton', value: 'ableton' },
+  { label: 'Adonis', value: 'adonis' },
+  { label: 'Deno', value: 'deno' },
+  { label: 'Django', value: 'django' },
+]
+
+const DBSkills = [
+  { label: 'MySQL', value: 'mysql' },
+  { label: 'Postgres', value: 'postgres' },
+  { label: 'Cassandra', value: 'cassandra' },
+  { label: 'Dynamodb', value: 'dynamodb' },
+]
+
+const otherSkills = [
+  { label: 'Ableton', value: 'ableton' },
+  { label: 'Activitypub', value: 'activitypub' },
+  { label: 'Androidstudio', value: 'androidstudio' },
+  { label: 'Ansible', value: 'ansible' },
+  { label: 'Apollo', value: 'apollo' },
+  { label: 'Appwrite', value: 'appwrite' },
+  { label: 'Arduino', value: 'arduino' },
+  { label: 'Atom', value: 'atom' }, // テキストエディタ
+  { label: 'AWS', value: 'aws' },
+  { label: 'Azul', value: 'azul' },
+  { label: 'Azure', value: 'azure' },
+  { label: 'Babel', value: 'babel' },
+  { label: 'Bash', value: 'bash' },
+  { label: 'Bevy', value: 'bevy' }, // Rustのゲームエンジン
+  { label: 'Blender', value: 'blender' },
+  { label: 'Bootstrap', value: 'bootstrap' },
+  { label: 'Bsd', value: 'bsd' },
+  { label: 'Cloudflare', value: 'cloudflare' },
+  { label: 'Cmake', value: 'cmake' },
+  { label: 'Codepen', value: 'codepen' },
+  { label: 'Docker', value: 'docker' },
+  { label: '.NET', value: 'dotnet' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
 ]
 
 const SkillIconsSelect = (props: Props) => {
@@ -59,7 +125,7 @@ const SkillIconsSelect = (props: Props) => {
     setQuery((prev) => ({ ...prev, icons: selectedSkillValues }))
   }, [selectedSkills, setQuery])
 
-  const onOchangeProgrammingSkills = (props: SelectSkill) => {
+  const onOchangeMySkills = (props: SelectSkill) => {
     const { skill, checked } = props
     if (checked) {
       selectProgramngSkills(skill)
@@ -112,31 +178,28 @@ const SkillIconsSelect = (props: Props) => {
         <CustomTabPanel value={value} index={0}>
           <CustomFormGroup
             skills={programmingSkills}
-            onChange={onOchangeProgrammingSkills}
+            onChange={onOchangeMySkills}
           />
         </CustomTabPanel>
         {/* TODO */}
-        {/* <CustomTabPanel value={value} index={1}>
+        <CustomTabPanel value={value} index={1}>
           <CustomFormGroup
             skills={frontendSkills}
-            onChange={onSelectFrontendSkills}
+            onChange={onOchangeMySkills}
           />
         </CustomTabPanel>
         <CustomTabPanel value={value} index={2}>
           <CustomFormGroup
             skills={backendSkills}
-            onChange={onSelectBackendSkills}
+            onChange={onOchangeMySkills}
           />
         </CustomTabPanel>
         <CustomTabPanel value={value} index={3}>
-          <CustomFormGroup skills={DBSkills} onChange={onSelectDBSkills} />
+          <CustomFormGroup skills={DBSkills} onChange={onOchangeMySkills} />
         </CustomTabPanel>
         <CustomTabPanel value={value} index={4}>
-          <CustomFormGroup
-            skills={otherSkills}
-            onChange={onSelectOtherSkills}
-          />
-        </CustomTabPanel> */}
+          <CustomFormGroup skills={otherSkills} onChange={onOchangeMySkills} />
+        </CustomTabPanel>
       </Paper>
     </Box>
   )

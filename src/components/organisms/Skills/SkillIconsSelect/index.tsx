63731ada3c58081f88a07342d35ec5ec95d0f788
kosuke-17 <tamutamu17@icloud.com>
2023-09-04 21:58:42 +0900

    定数ファイルに分離

--- src/components/organisms/Skills/SkillIconsSelect/index.tsx
+++ src/components/organisms/Skills/SkillIconsSelect/index.tsx
@@ -12,6 +12,13 @@ import type { Dispatch, SetStateAction, SyntheticEvent } from 'react'
 import { useEffect, useState } from 'react'
 
 import type { SkillQuery } from '@/components/templates/Skills'
+import {
+  backendSkills,
+  DBSkills,
+  frontendSkills,
+  otherSkills,
+  programmingSkills,
+} from '@/constants/skills'
 
 type Props = {
   title?: string
@@ -30,80 +37,6 @@ const tabs = [
   { label: 'バックエンド', value: 2 },
   { label: 'DB', value: 3 },
   { label: 'その他', value: 4 },
-]
-
-const programmingSkills = [
-  { label: 'Aiscript', value: 'aiscript' },
-  { label: 'TypeScript', value: 'ts' },
-  { label: 'Javascript', value: 'js' },
-  { label: 'Python', value: 'py' },
-  { label: 'Java', value: 'java' },
-  { label: 'PHP', value: 'php' },
-  { label: 'C', value: 'c' },
-  { label: 'C#', value: 'cs' },
-  { label: 'C++', value: 'cpp' },
-  { label: 'Crystal', value: 'crystal' },
-  { label: 'Clojure', value: 'clojure' },
-  { label: 'Coffeescript', value: 'coffeescript' },
-  { label: 'CSS', value: 'css' },
-  { label: 'Dart', value: 'dart' },
-  { label: '', value: '' },
-]
-
-const frontendSkills = [
-  { label: 'Alpinejs', value: 'alpinejs' },
-  { label: 'Angular', value: 'angular' },
-  { label: 'Astro', value: 'astro' },
-  { label: 'D3', value: 'd3' },
-  { label: '', value: '' },
-]
-const backendSkills = [
-  { label: 'Actix', value: 'actix' }, // rustのフレームワーク
-  { label: 'Ableton', value: 'ableton' },
-  { label: 'Adonis', value: 'adonis' },
-  { label: 'Deno', value: 'deno' },
-  { label: 'Django', value: 'django' },
-]
-
-const DBSkills = [
-  { label: 'MySQL', value: 'mysql' },
-  { label: 'Postgres', value: 'postgres' },
-  { label: 'Cassandra', value: 'cassandra' },
-  { label: 'Dynamodb', value: 'dynamodb' },
-]
-
-const otherSkills = [
-  { label: 'Ableton', value: 'ableton' },
-  { label: 'Activitypub', value: 'activitypub' },
-  { label: 'Androidstudio', value: 'androidstudio' },
-  { label: 'Ansible', value: 'ansible' },
-  { label: 'Apollo', value: 'apollo' },
-  { label: 'Appwrite', value: 'appwrite' },
-  { label: 'Arduino', value: 'arduino' },
-  { label: 'Atom', value: 'atom' }, // テキストエディタ
-  { label: 'AWS', value: 'aws' },
-  { label: 'Azul', value: 'azul' },
-  { label: 'Azure', value: 'azure' },
-  { label: 'Babel', value: 'babel' },
-  { label: 'Bash', value: 'bash' },
-  { label: 'Bevy', value: 'bevy' }, // Rustのゲームエンジン
-  { label: 'Blender', value: 'blender' },
-  { label: 'Bootstrap', value: 'bootstrap' },
-  { label: 'Bsd', value: 'bsd' },
-  { label: 'Cloudflare', value: 'cloudflare' },
-  { label: 'Cmake', value: 'cmake' },
-  { label: 'Codepen', value: 'codepen' },
-  { label: 'Docker', value: 'docker' },
-  { label: '.NET', value: 'dotnet' },
-  { label: '', value: '' },
-  { label: '', value: '' },
-  { label: '', value: '' },
-  { label: '', value: '' },
-  { label: '', value: '' },
-  { label: '', value: '' },
-  { label: '', value: '' },
-  { label: '', value: '' },
-  { label: '', value: '' },
 ]
 
 const SkillIconsSelect = (props: Props) => {
---
+++ src/constants/skills.ts
@@ -0,0 +1,73 @@
+export const programmingSkills = [
+  { label: 'Aiscript', value: 'aiscript' },
+  { label: 'TypeScript', value: 'ts' },
+  { label: 'Javascript', value: 'js' },
+  { label: 'Python', value: 'py' },
+  { label: 'Java', value: 'java' },
+  { label: 'PHP', value: 'php' },
+  { label: 'C', value: 'c' },
+  { label: 'C#', value: 'cs' },
+  { label: 'C++', value: 'cpp' },
+  { label: 'Crystal', value: 'crystal' },
+  { label: 'Clojure', value: 'clojure' },
+  { label: 'Coffeescript', value: 'coffeescript' },
+  { label: 'CSS', value: 'css' },
+  { label: 'Dart', value: 'dart' },
+  { label: '', value: '' },
+]
+
+export const frontendSkills = [
+  { label: 'Alpinejs', value: 'alpinejs' },
+  { label: 'Angular', value: 'angular' },
+  { label: 'Astro', value: 'astro' },
+  { label: 'D3', value: 'd3' },
+  { label: '', value: '' },
+]
+export const backendSkills = [
+  { label: 'Actix', value: 'actix' }, // rustのフレームワーク
+  { label: 'Ableton', value: 'ableton' },
+  { label: 'Adonis', value: 'adonis' },
+  { label: 'Deno', value: 'deno' },
+  { label: 'Django', value: 'django' },
+]
+
+export const DBSkills = [
+  { label: 'MySQL', value: 'mysql' },
+  { label: 'Postgres', value: 'postgres' },
+  { label: 'Cassandra', value: 'cassandra' },
+  { label: 'Dynamodb', value: 'dynamodb' },
+]
+
+export const otherSkills = [
+  { label: 'Ableton', value: 'ableton' },
+  { label: 'Activitypub', value: 'activitypub' },
+  { label: 'Androidstudio', value: 'androidstudio' },
+  { label: 'Ansible', value: 'ansible' },
+  { label: 'Apollo', value: 'apollo' },
+  { label: 'Appwrite', value: 'appwrite' },
+  { label: 'Arduino', value: 'arduino' },
+  { label: 'Atom', value: 'atom' }, // テキストエディタ
+  { label: 'AWS', value: 'aws' },
+  { label: 'Azul', value: 'azul' },
+  { label: 'Azure', value: 'azure' },
+  { label: 'Babel', value: 'babel' },
+  { label: 'Bash', value: 'bash' },
+  { label: 'Bevy', value: 'bevy' }, // Rustのゲームエンジン
+  { label: 'Blender', value: 'blender' },
+  { label: 'Bootstrap', value: 'bootstrap' },
+  { label: 'Bsd', value: 'bsd' },
+  { label: 'Cloudflare', value: 'cloudflare' },
+  { label: 'Cmake', value: 'cmake' },
+  { label: 'Codepen', value: 'codepen' },
+  { label: 'Docker', value: 'docker' },
+  { label: '.NET', value: 'dotnet' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+  { label: '', value: '' },
+]
